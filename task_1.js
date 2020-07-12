3bfb0d54474b6d86e76129ed3928ad2edd91e3e0
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-07-12 14:24:53 +0300

    HW-8 res

--- task_1.js
+++ task_1.js
@@ -1,48 +1,67 @@
-import gallery_items from '/JS-home_work-8/gallery_items.js'
+import gallery_items from "/JS-home_work-8/gallery_items.js";
 
 const ref = {
-    galleryBox: document.querySelector('.js-gallery'),
-    lightBox: document.querySelector('.js-lightbox'),
-    galleryLink: document.querySelector('.gallery__link'),
-    largeImg: document.querySelector('.lightbox__image'),
-    buttonClose: document.querySelector('.lightbox__button'),
+    galleryBox: document.querySelector(".js-gallery"),
+    lightBox: document.querySelector(".js-lightbox"),
+    galleryLink: document.querySelector(".gallery__link"),
+    largeImg: document.querySelector(".lightbox__image"),
+    buttonClose: document.querySelector(".lightbox__button"),
+};
+
+ref.galleryBox.addEventListener("click", openModal);
+ref.lightBox.addEventListener("click", closeModalByOverlay);
+ref.buttonClose.addEventListener("click", closeModal);
+window.addEventListener('keydown', closeModalByEsc)
+function createMarkup() {
+    const markup = gallery_items.map((el) => {
+       return `<li class="gallery__item">
+          <a
+          class="gallery__link"
+          href="#"
+          >
+          <img
+          class="gallery__image"
+          src="${el.preview}"
+          data-source="${el.original}"
+          alt="${el.description}"
+          />
+          </a>
+          </li>`;
+        }).join('');
+        return markup;
+        
 }
-gallery_items.map(el => {
-    ref.galleryBox.insertAdjacentHTML('beforeend', `<li class="gallery__item">
-    <a
-    class="gallery__link"
-    href="#"
-    >
-    <img
-    class="gallery__image"
-    src="${el.preview}"
-    data-source="${el.original}"
-    alt="${el.description}"
-    />
-    </a>
-    </li>`)
-});
-ref.galleryBox.addEventListener('click', openModal);
-ref.lightBox.addEventListener('click', closeModal)
-ref.buttonClose.addEventListener('click', closeModalBtn)
+
+function renderItems(markup) {
+    ref.galleryBox.insertAdjacentHTML('beforeend', markup)
+}
+renderItems(createMarkup());
+
 
 function openModal(event) {
-    event.preventDefault()
-    if (event.target.nodeName !== 'IMG') {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
         return;
     }
     const bigImg = event.target.dataset.source;
-    ref.lightBox.classList.add('is-open');
+    ref.lightBox.classList.add("is-open");
     ref.largeImg.src = bigImg;
 }
-function closeModal(event) {
-    if (event.target === document.querySelector('.lightbox__content')) {
-        ref.largeImg.src = "#";
-        ref.lightBox.classList.remove('is-open');
-    }
 
+function closeModalByOverlay(event) {
+    if (event.target === document.querySelector(".lightbox__content")) {
+console.log(event.target);
+console.log(event.currentTarget);
+        closeModal()
+    }
 }
-function closeModalBtn() {
+function closeModal() {
     ref.largeImg.src = "#";
-    ref.lightBox.classList.remove('is-open');
+    ref.lightBox.classList.remove("is-open");
 }
+function closeModalByEsc(event){
+    if (event.code !== "Escape") {
+        return;
+      }
+      closeModal();
+    };
